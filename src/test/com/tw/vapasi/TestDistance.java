3f6156ef1163d51553eacc77d3e6adce44291386
Merlin <merlin.p.varghese@gmail.com>
2019-09-02 23:18:09 +0530

    [Vidya/Merlin]Add. Added more test scenarios

--- src/test/com/tw/vapasi/TestDistance.java
+++ src/test/com/tw/vapasi/TestDistance.java
@@ -2,8 +2,7 @@ package com.tw.vapasi;
 
 import org.junit.jupiter.api.Test;
 
-import static com.tw.vapasi.Distance.cms;
-import static com.tw.vapasi.Distance.meter;
+import static com.tw.vapasi.Distance.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestDistance {
@@ -15,7 +14,28 @@ class TestDistance {
 
   @Test
   void expect90CmNotEquals1m() {
-    assertFalse(cms(90).equals(meter(1)));
-
+    assertNotEquals(cms(90),meter(1));
   }
+
+  @Test
+  void expect100000CmEquals1Kilometer() {
+    assertEquals(cms(100000), km(1));
+  }
+
+  @Test
+  void expect90000CmEquals1Kilometer() {
+    assertNotEquals(cms(90000), km(1));
+  }
+
+  @Test
+  void expect5mEquals500Centimeter() {
+    assertEquals(meter(5), cms(500));
+  }
+
+  @Test
+  void expect7000MeterEquals8Kilometer() {
+    assertNotEquals(meter(7000), km(8));
+  }
+
+
 }
